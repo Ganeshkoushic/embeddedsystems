0fed27aec0b5e53190359bb5b1a7689e63f1e794
Ganeshkoushic <49732445+Ganeshkoushic@users.noreply.github.com>
2020-01-01 19:16:36 -0600

    Added code to demonstrate LEDs blinking on MSP430

---
+++ blinkingLeds/main.c
@@ -0,0 +1,32 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1DIR|=0x41;
+	P1REN|=0x08;
+	long unsigned int i;
+	while(1)
+	{
+	    if((P1IN & 0x08))
+	    {
+	        P1OUT|=0x01;             //Toggle LED
+	        for(i=0;i<400000;i++);//delay
+	        P1OUT&=0xFE;
+	        for(i=0;i<400000;i++);
+	    }
+	    else
+	        {
+	            P1OUT|=0x40;             //Toggle LED
+	            for(i=0;i<400000;i++);     //delay
+	            P1OUT&=0xBF;
+	            for(i=0;i<400000;i++);
+	        }
+
+	    }
+	return 0;
+}
